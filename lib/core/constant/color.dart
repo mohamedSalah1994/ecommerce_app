b01c931fa36cc0d770f6c744bcfa8d13315e8075
MohamedSalah <elkomymohamed69@gmail.com>
2023-05-29 14:32:34 +0300

    working on onBoarding page

---
+++ lib/core/constant/color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+
+  
+
+  static  Color grey = const Color(0xff8e8e8e);
+  
+}
---
+++ lib/data/model/onBoardingModel.dart
@@ -0,0 +1,9 @@
+class OnBoardingModel {
+  final String? title;
+  final String? body;
+  final String? image;
+
+  OnBoardingModel({this.title, this.body, this.image});
+
+  
+}
